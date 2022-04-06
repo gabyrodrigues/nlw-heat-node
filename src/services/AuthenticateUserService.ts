424965582390c042434d4a9bd4b679f9f52ab379
gabyrodrigues <gabyrlima.99@gmail.com>
2022-04-05 23:29:44 -0300

    authenticate user on database and return token

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -9,6 +9,8 @@
 */
 
 import axios from 'axios'
+import { sign } from 'jsonwebtoken'
+import prismaClient from '../prisma'
 
 interface IAccessTokenResponse {
 	access_token: string
@@ -42,7 +44,41 @@ class AuthenticateUserService {
 			}
 		})
 
-		return response.data
+		const { login, id, avatar_url, name } = response.data
+
+		let user = await prismaClient.user.findFirst({
+			where: {
+				github_id: id
+			}
+		})
+
+		if(!user) {
+			user = await prismaClient.user.create({
+				data: {
+					github_id: id,
+					login,
+					avatar_url,
+					name
+				}
+			})
+		}
+
+		const token = sign( 
+			{
+				user: {
+					name: user.name,
+					avatar_url: user.avatar_url,
+					id: user.id
+				}
+			},
+			process.env.JWT_SECRET,
+			{
+				subject: user.id,
+				expiresIn: "1d"
+			}
+		)
+
+		return { token, user }
 	}
 }
 
